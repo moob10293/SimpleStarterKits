884d869100973a520abe9e516c0775544242decc
BuildTools <unconfigured@null.spigotmc.org>
2022-02-27 10:24:56 -0800

    changed default starter kit

--- src/main/java/me/noob/starterkits/Main.java
+++ src/main/java/me/noob/starterkits/Main.java
@@ -1,20 +1,14 @@
 package me.noob.starterkits;
 
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-//use something.getdatafolder
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        // Plugin startup logic
         this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new PlayerSpawnEvent(this.getConfig()), this);
     }
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
