c2f713fd4ad7360e0c99ac6f93ce1f486389efd3
noob10293 <moob10293@gmail.com>
2024-06-11 12:46:49 -0700

    Fixed the way starterkits were stored

--- src/main/java/me/noob/simplestarterkits/KitManager.java
+++ src/main/java/me/noob/simplestarterkits/KitManager.java
@@ -1,5 +1,6 @@
 package me.noob.simplestarterkits;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -7,10 +8,13 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class KitManager extends ConfigurationManager {
 
-    private final ConfigurationManager pluginConfig = SimpleStarterKits.getConfigManager();
-    private PlayerInventory starterKit;
+    private ConfigurationManager pluginConfig;
+    private List<ItemStack> starterKit = new ArrayList<>();
 
     public KitManager(String fileName, @NotNull JavaPlugin plugin) {
         super(fileName, plugin);
@@ -18,32 +22,56 @@ public class KitManager extends ConfigurationManager {
 
     public void giveStarterKit(@NotNull Player player) {
         int index = 0;
+        boolean messaged = false;
         PlayerInventory playerInventory = player.getInventory();
         for (ItemStack itemStack : starterKit) {
-            if (itemStack == null) {
+//          empty itemstacks are random btwn null and air
+            if (itemStack == null||itemStack.getType()== Material.AIR) {
                 index++;
                 continue;
             }
-            if (playerInventory.getItem(index) != null) {
+//            already checks if null
+//            noinspection DataFlowIssue
+            if (playerInventory.getItem(index) == null || playerInventory.getItem(index).getType() == Material.AIR) {
+                player.getInventory().setItem(index, itemStack);
+                index++;
+                continue;
+            }
+            if (!messaged) {
                 player.sendMessage(pluginConfig.get("conflict-message"));
-                Item item = player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
-                item.setGlowing(true);
-                index++;
-                continue;
+                messaged=true;
             }
-            player.getInventory().setItem(index, itemStack);
+            Item item = player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
+            item.setGlowing(true);
             index++;
         }
     }
 
     public void setStarterKit(PlayerInventory inventory) {
-        starterKit = inventory;
-        set(pluginConfig.get("starter-kit-name"), inventory);
+//      todo: replace with actual deep copy in case of bugs like amount still carrying over
+        starterKit = Arrays.stream(inventory.getContents())
+                .map(item -> item != null ? item.clone() : null)
+                .toList();
+        set(pluginConfig.get("starter-kit-name"), starterKit);
     }
 
     @Override
     public void reload() {
         super.reload();
-        starterKit = get(pluginConfig.get("starter-kit-name"), PlayerInventory.class);
+        pluginConfig = SimpleStarterKits.getConfigManager();
+        // TODO: Replace with simpler list comprehension idk how
+//        this throws error if it's null or can't cast
+//        I have no idea how it works
+        starterKit = Objects.requireNonNull(config.getList(pluginConfig.get("starter-kit-name")))
+                .stream()
+                .map(ItemStack.class::cast)
+                .collect(Collectors.toList());
+//        this causes warnings for some reason
+//        try {
+//            starterKit = (List<ItemStack>) config.getList(pluginConfig.get("starter-kit-name"));
+//        } catch (ClassCastException e){
+//            e.printStackTrace();
+//            logger.severe("Path '" + pluginConfig.get("starter-kit-name") + "' in file " + fileName + " is not a list!");
+//        }
     }
 }
